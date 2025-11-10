eb7c3f5967a8da03eafca2659cbfabee7ce4d2b4
unknown <wkdqhrhkd12@gmail.com>
2025-11-11 01:15:20 +0900

    asset :  이미지 추가

--- basic.js
+++ basic.js
@@ -145,15 +145,6 @@
     state.time = Math.max(0, state.time - 3);
   }
 
-  const characterData = [
-      { src: "asset/img/angry.png", name: "화난 범뮤" },
-      { src: "asset/img/cowboy.png", name: "카우보이 범뮤" },
-      { src: "asset/img/cyborg.png", name: "사이보그 범뮤" },
-      { src: "asset/img/normal.png", name: "평범한 범뮤" },
-      { src: "asset/img/idol.png", name: "아이돌 범뮤" },
-      { src: "asset/img/hanbok.png", name: "한복 범뮤" }
-  ];
-
   function endGame() {
     state.playing = false;
     clearInterval(state.timerId);
---
+++ image-loader.js
@@ -0,0 +1,13 @@
+const characterData = [
+    { src: "asset/img/angry.png", name: "화난 범뮤" },
+    { src: "asset/img/cowboy.png", name: "카우보이 범뮤" },
+    { src: "asset/img/cyborg.png", name: "사이보그 범뮤" },
+    { src: "asset/img/hanbok.png", name: "한복 범뮤" },
+    { src: "asset/img/idol.png", name: "아이돌 범뮤" },
+    { src: "asset/img/knight.png", name: "기사 범뮤" },
+    { src: "asset/img/normal.png", name: "평범한 범뮤" },
+    { src: "asset/img/prince.png", name: "왕자 범뮤" },
+    { src: "asset/img/princess.png", name: "공주 범뮤" },
+    { src: "asset/img/student.png", name: "학생 범뮤" },
+    { src: "asset/img/summer.png", name: "여름 범뮤" }
+];
